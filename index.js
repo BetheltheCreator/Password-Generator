e0dc5faa66e7c31af0fab9045419fd4df7e586ef
BetheltheCreator <bethelanozie4@gmail.com>
2025-05-20 06:43:47 -0700

    commit initial files

---
+++ index.js
@@ -0,0 +1,25 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let viewOneEl = document.getElementById("fieldone")
+let viewTwoEl = document.getElementById("fieldtwo")
+let passwordLength = 15
+
+function generateone(){
+    let index = Math.floor(Math.random()* characters.length)
+    return characters[index]
+}
+
+function generateRandomPassword(){
+    let passKey = ""
+    for (let i = 0; i < passwordLength; i ++ ){
+        passKey += generateone()
+    }
+    return passKey
+    
+}
+
+
+function generate() {
+    viewOneEl.textContent = generateRandomPassword()
+    viewTwoEl.textContent = generateRandomPassword()
+}
